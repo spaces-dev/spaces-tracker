0db3b9c21273560cfd2c5e532d326d5375c1b894
Vitalij Ryndin <crashmax.dev@gmail.com>
2025-12-31 02:36:19 +0000

    chore: Changed 1 file(s)
    - JS/acl.js

--- JS/acl.js
+++ JS/acl.js
@@ -6,11 +6,11 @@ var ACL_MODES = {
 	BY_ACCESS_LIST: 5
 };
 
-module.on("_componentpage", function () {
+module.on("componentpage", function () {
 	$('#main').on('change', '.js-acl_dropdown .js-radio input', function (e) {
 		var el = $(this),
 			wrap = el.parents('.js-acl_dropdown');
-
+		
 		var id = wrap.data('name') + wrap.data('postfix');
 		$('#passwd_' + id).toggleClass('hide', el.val() != ACL_MODES.BY_PASSWORD);
 		$('#acclist_' + id).toggleClass('hide', el.val() != ACL_MODES.BY_ACCESS_LIST);
