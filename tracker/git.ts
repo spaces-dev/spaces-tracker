69f3ad7def5a448d508b0b1a0f7bed60530a17ef
Vitalij Ryndin <crashmax.dev@gmail.com>
2026-01-16 23:18:03 +0300

    feat: commit/push, send message to telegram from script (#8)

---
+++ tracker/git.ts
@@ -0,0 +1,22 @@
+import { execCmd } from './utils.ts'
+
+export async function commitAndPush(message: string) {
+  if (!process.env.CI) {
+    console.log('\nNot in CI, skipping commit and push')
+    return '998fe5c' // for debugging `sendNotifications`
+  }
+
+  await execCmd('git', ['add', '.'])
+  const { stdout: status } = await execCmd('git', ['status', '--porcelain'])
+  if (!status.trim()) return
+
+  console.log('Changes detected. Committing...')
+
+  await execCmd('git', ['config', 'user.name', 'Vitalij Ryndin'])
+  await execCmd('git', ['config', 'user.email', 'crashmax.dev@gmail.com'])
+  await execCmd('git', ['commit', '-m', message])
+  await execCmd('git', ['push'])
+
+  const { stdout: sha } = await execCmd('git', ['rev-parse', '--short', 'HEAD'])
+  return sha.trim()
+}
