d149a971625485c8a1176456466ba0a7d407a408
Vitalij Ryndin <crashmax.dev@gmail.com>
2026-04-03 11:34:09 +0000

    chore: Changed 1 file(s) Changed files (1): spaces/JS/spacesLib.js | 47.1 kB (-4.3 kB) | 13 hours ago
    Summary (z-ai/glm-4.5-air:free):
    Добавлена проверка существования ключа CK в объекте params перед его проверкой. Теперь значение по умолчанию устанавливается только если ключ существует и его значение ложно. Это предотвращает ошибку ReferenceError и случайную перезапись существующего параметра.

--- spaces/JS/spacesLib.js
+++ spaces/JS/spacesLib.js
@@ -603,7 +603,7 @@ extend(Spaces, {
 
 		params._origin = location.protocol + "//" + location.host
 
-		if (!params.CK)
+		if (('CK' in params) && !params.CK)
 			params.CK = Spaces.CK();
 
 		var raw_data = Url.buildQuery(params);
